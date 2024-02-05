2896deec3a16ff4bbe37169b23b2145989ab2060
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2024-02-06 00:29:53 +0530

    changes in code and added sphere function

--- p1.py
+++ p1.py
@@ -14,6 +14,8 @@ def sphere_function(individual):
 def binary_social_group_optimization(N = 10, D = 30, LL = -100, UL = 100, g = 10, c=0.2):
     # 1. Initialize the population
     population = np.random.randint(LL, UL, size=(N, D))  # Generate binary population
+    print(population)
+    gBest_array = []
 
     for gen in range(g):
         # 2. Calculate fitness of the population
@@ -40,7 +42,7 @@ def binary_social_group_optimization(N = 10, D = 30, LL = -100, UL = 100, g = 10
                 r = np.random.rand()
                 Xnew = c * population[i, j] + r * (gbest[j] - population[i, j])
                 temp[j] = Xnew
-                if sphere_function(temp) <= fitness[i]:  # Accept if better
+                if sphere_function(temp) < fitness[i]:  # Accept if better
                     population[i, j] = Xnew
 
         # 4. Acquiring Phase (Corrected while loop condition)
@@ -48,31 +50,31 @@ def binary_social_group_optimization(N = 10, D = 30, LL = -100, UL = 100, g = 10
             # Randomly select a different person (using np.any for element-wise comparison)
             ind = np.random.randint(0, N)
             Xr = population[ind]
-            while np.any(Xr == population[i]):  # Ensure selection of a distinct individual
-                ind = np.random.randint(0, N)
-                Xr = population[ind]
+            # while np.any(Xr == population[i]):  # Ensure selection of a distinct individual
+            #     ind = np.random.randint(0, N)
+            #     Xr = population[ind]
             temp = population[i]
             if fitness[i] < fitness[ind]:  # Learn from more knowledgeable
                 r1, r2 = np.random.rand(2)
                 for j in range(D):
                     Xnew = population[i, j] + r1 * (population[i, j] - Xr[j]) + r2 * (gbest[j] - population[i, j])
                     temp[j] = Xnew
-                    if sphere_function(temp) <= fitness[i]:
+                    if sphere_function(temp) < fitness[i]:
                         population[i, j] = Xnew
             else:  # Learn from those with better attributes
                 r1, r2 = np.random.rand(2)
                 for j in range(D):
                     Xnew = population[i, j] + r1 * (Xr[j] - population[i, j]) + r2 * (gbest[j] - population[i, j])
                     temp[j] = Xnew
-                    if sphere_function(temp) <= fitness[i]:
+                    if sphere_function(temp) < fitness[i]:
                         population[i, j] = Xnew
+        gBest_array.append(gbest)
 
     # 5. Termination criterion
 
     best_solution = population[np.argmin(fitness)]
     best_fitness = sphere_function(best_solution)
-    print(best_solution)
-    print(best_fitness)
+    print(gBest_array)
     return best_solution, best_fitness
 
 binary_social_group_optimization()
