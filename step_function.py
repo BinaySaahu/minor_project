9a3e132ebeb4a19bc773ea5a7fa18390b17b8406
Purnendu Thamb <pthamb2002@gmail.com>
2024-02-12 00:44:44 +0530

    changed the sum square function and the step fun

--- step_function.py
+++ step_function.py
@@ -1,80 +1,47 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def sphere_function(individual):
-    fitness_value = 0
-    for i in individual:
-        if i < -100:
-            i = -100
-        if i > 100:
-            i = 100
-        fitness_value+=i**2
-    return fitness_value
-          
+# New step function for fitness evaluation
+def step_function(individual):
+    return sum(int(round(xi)) ** 2 for xi in individual)
 
-def binary_social_group_optimization(N = 10, D = 30, LL = -100, UL = 100, g = 10, c=0.2):
-    # 1. Initialize the population
-    population = np.random.randint(LL, UL, size=(N, D))  # Generate binary population
-    print(population)
-    gBest_array = []
-
+def binary_social_group_optimization(N=10, D=30, LL=-100, UL=100, g=10, c=0.2):
+    population = np.random.randint(LL, UL, size=(N, D))
+    print("Initial Population:\n", population)
+    
+    best_fitness_over_generations = []  # To track the best fitness value over generations
+    
     for gen in range(g):
-        # 2. Calculate fitness of the population
-        fitness_values = []
-
-        # Loop through each individual in the population
-        for individual in population:
-            # Calculate the fitness value for the current individual
-            individual_fitness = sphere_function(individual)
-            # Append the calculated fitness value to the list
-            fitness_values.append(individual_fitness)
-
-        # Convert the list of fitness values into a NumPy array for efficient operations
+        # Calculate fitness of the population using the new step function
+        fitness_values = [step_function(individual) for individual in population]
         fitness = np.array(fitness_values)
-
-        # print(fitness)
-
-        # 3. Improving Phase
-        gbest = population[np.argmin(fitness)]  # Find the best solution
-
+        
+        gbest_index = np.argmin(fitness)
+        gbest = population[gbest_index]
+        best_fitness_over_generations.append(fitness[gbest_index])  # Track the best fitness
+        
+        # Improving Phase
         for i in range(N):
-            temp = population[i]
             for j in range(D):
                 r = np.random.rand()
                 Xnew = c * population[i, j] + r * (gbest[j] - population[i, j])
-                temp[j] = Xnew
-                if sphere_function(temp) < fitness[i]:  # Accept if better
-                    population[i, j] = Xnew
+                population[i, j] = Xnew  # Directly updating without re-evaluation step
+        
+        # Acquiring Phase
+        # Simplified for demonstration purposes; directly updating without re-evaluation step
 
-        # 4. Acquiring Phase (Corrected while loop condition)
-        for i in range(N):
-            # Randomly select a different person (using np.any for element-wise comparison)
-            ind = np.random.randint(0, N)
-            Xr = population[ind]
-            # while np.any(Xr == population[i]):  # Ensure selection of a distinct individual
-            #     ind = np.random.randint(0, N)
-            #     Xr = population[ind]
-            temp = population[i]
-            if fitness[i] < fitness[ind]:  # Learn from more knowledgeable
-                r1, r2 = np.random.rand(2)
-                for j in range(D):
-                    Xnew = population[i, j] + r1 * (population[i, j] - Xr[j]) + r2 * (gbest[j] - population[i, j])
-                    temp[j] = Xnew
-                    if sphere_function(temp) < fitness[i]:
-                        population[i, j] = Xnew
-            else:  # Learn from those with better attributes
-                r1, r2 = np.random.rand(2)
-                for j in range(D):
-                    Xnew = population[i, j] + r1 * (Xr[j] - population[i, j]) + r2 * (gbest[j] - population[i, j])
-                    temp[j] = Xnew
-                    if sphere_function(temp) < fitness[i]:
-                        population[i, j] = Xnew
-        gBest_array.append(gbest)
-
-    # 5. Termination criterion
-
-    best_solution = population[np.argmin(fitness)]
-    best_fitness = sphere_function(best_solution)
-    print(gBest_array)
+    # Plotting the optimization process
+    plt.plot(best_fitness_over_generations, marker='o', linestyle='-', color='blue')
+    plt.title('Best Fitness over Generations')
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness Value')
+    plt.grid(True)
+    plt.show()
+    
+    best_solution = population[gbest_index]
+    best_fitness = step_function(best_solution)
+    print("Best Solution:", best_solution)
+    print("Best Fitness:", best_fitness)
     return best_solution, best_fitness
 
 binary_social_group_optimization()
--- sumsquare_func.py
+++ sumsquare_func.py
@@ -1,86 +1,64 @@
 import numpy as np
-
-def sphere_function(individual):
-    fitness_value = 0
-    for i in individual:
-        if i < -100:
-            i = -100
-        elif i > 100:
-            i = 100
-        fitness_value += i**2
-    return fitness_value
+import matplotlib.pyplot as plt
 
 def sum_squares_function(individual):
     return sum(i * (xi ** 2) for i, xi in enumerate(individual, start=1))
 
 def combined_fitness_function(individual):
-    return sphere_function(individual), sum_squares_function(individual)
+    return sum_squares_function(individual)
 
 def binary_social_group_optimization(N = 10, D = 30, LL = -100, UL = 100, g = 10, c=0.2):
-    # 1. Initialize the population
     population = np.random.randint(LL, UL, size=(N, D))
     print("Initial population:\n", population)
-    gBest_array = []
-
+    
+    best_fitness_over_generations = []  # To track the best fitness value over generations
+    
     for gen in range(g):
-        # 2. Calculate fitness of the population
         fitness_values = [combined_fitness_function(individual) for individual in population]
         fitness = np.array(fitness_values)
-
-        # 3. Improving Phase
-        gbest_indices = np.argmin(fitness, axis=0)
-        gbest_sphere = population[gbest_indices[0]]
-        gbest_sum_squares = population[gbest_indices[1]]
-
+        
+        gbest_index = np.argmin(fitness)
+        best_fitness_over_generations.append(fitness[gbest_index])  # Track the best fitness
+        
+        gbest = population[gbest_index]
+        
         for i in range(N):
             temp = population[i].copy()
             for j in range(D):
                 r = np.random.rand()
-                # Sphere function improvement
-                Xnew_sphere = c * population[i, j] + r * (gbest_sphere[j] - population[i, j])
-                temp[j] = Xnew_sphere
-                if combined_fitness_function(temp)[0] < fitness[i][0]:  # Accept if better in sphere function
-                    population[i, j] = Xnew_sphere
+                Xnew = c * population[i, j] + r * (gbest[j] - population[i, j])
+                temp[j] = Xnew
+                if combined_fitness_function(temp) < fitness[i]:
+                    population[i, j] = Xnew
 
-                # Sum squares function improvement
-                Xnew_sum_squares = c * population[i, j] + r * (gbest_sum_squares[j] - population[i, j])
-                temp[j] = Xnew_sum_squares
-                if combined_fitness_function(temp)[1] < fitness[i][1]:  # Accept if better in sum squares function
-                    population[i, j] = Xnew_sum_squares
-
-        # 4. Acquiring Phase
         for i in range(N):
             ind = np.random.randint(0, N)
             Xr = population[ind]
             temp = population[i].copy()
-            if fitness[i][0] < fitness[ind][0]:  # Learn from more knowledgeable in sphere function
+            if fitness[i] < fitness[ind]:
                 r1, r2 = np.random.rand(2)
                 for j in range(D):
-                    Xnew = population[i, j] + r1 * (population[i, j] - Xr[j]) + r2 * (gbest_sphere[j] - population[i, j])
+                    Xnew = population[i, j] + r1 * (population[i, j] - Xr[j]) + r2 * (gbest[j] - population[i, j])
                     temp[j] = Xnew
-                    if combined_fitness_function(temp)[0] < fitness[i][0]:
+                    if combined_fitness_function(temp) < fitness[i]:
                         population[i, j] = Xnew
 
-            if fitness[i][1] < fitness[ind][1]:  # Learn from more knowledgeable in sum squares function
-                r1, r2 = np.random.rand(2)
-                for j in range(D):
-                    Xnew = population[i, j] + r1 * (Xr[j] - population[i, j]) + r2 * (gbest_sum_squares[j] - population[i, j])
-                    temp[j] = Xnew
-                    if combined_fitness_function(temp)[1] < fitness[i][1]:
-                        population[i, j] = Xnew
+    best_solution = population[gbest_index]
+    best_fitness = sum_squares_function(best_solution)
 
-        gBest_array.append((gbest_sphere, gbest_sum_squares))
+    print("Best Solution:", best_solution)
+    print("Best Fitness:", best_fitness)
 
-    # 5. Termination criterion
-    best_sphere_solution = population[gbest_indices[0]]
-    best_sum_squares_solution = population[gbest_indices[1]]
-    best_sphere_fitness = sphere_function(best_sphere_solution)
-    best_sum_squares_fitness = sum_squares_function(best_sum_squares_solution)
-
-    print("gBest array:\n", gBest_array)
-    return (best_sphere_solution, best_sphere_fitness), (best_sum_squares_solution, best_sum_squares_fitness)
+    # Return the best solution, fitness, and the tracking of fitness over generations
+    return best_solution, best_fitness, best_fitness_over_generations
 
 # Running the optimization
-best_sphere, best_sum_squares = binary_social_group_optimization()
-print("Best Sphere Solution:", best_sphere)
-print("Best Sum Squares Solution:", best_sum_squares)
+best_solution, best_fitness, best_fitness_over_generations = binary_social_group_optimization()
+
+# Plotting the best fitness over generations
+plt.plot(best_fitness_over_generations, marker='o')
+plt.title('Best Fitness Over Generations')
+plt.xlabel('Generation')
+plt.ylabel('Best Fitness')
+plt.grid(True)
+plt.show()
